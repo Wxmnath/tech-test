c9e0d9e7b99b5a0ed92b52803e471bd1efc39d07
Wxmnath <Wxmnath@aol.com>
2022-03-29 20:36:51 +0100

    getImages added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "../styles/Search.css";
+import getImages from "../requests/getImages";
 
 function SearchForm() {
   const [value, setValue] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(value);
+  };
+
   return (
     <>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           className="search-input"
           type="text"
