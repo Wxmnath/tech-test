57c64d07bbe2552b3d9e20de566194b2742bf778
Wxmnath <Wxmnath@aol.com>
2022-04-01 17:00:32 +0100

    function added to return multiple images

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -7,13 +7,14 @@ function SearchResults({ results }) {
   } else {
     return (
       <>
-        <p className="search-result"> Search Results: </p>
-        <img
-          className="search-results"
-          src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-          aria-hidden
-          alt="space-image"
-        />
+        {results.map((image) => (
+          <img
+            className="search-results"
+            src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+            aria-hidden
+            alt="space-image"
+          />
+        ))}
       </>
     );
   }
